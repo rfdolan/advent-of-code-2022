abe543e5d0b323b032d9ef42920c6f3f73735a15
Raymond <rfdolan@wpi.edu>
2022-12-03 21:37:43 -0800

    Day 04 quick

--- src/bin/day04.rs
+++ src/bin/day04.rs
@@ -10,36 +10,51 @@ fn main(){
     solve(&vec);
 }
 
-fn do_overlap(pair1: Section, pair2: Section) -> bool {
+fn do_overlap(pair1: &Section, pair2: &Section) -> bool {
 	return pair1.min <= pair2.max && pair2.min <= pair1.max 
+}
+
+fn contains(pair1: &Section, pair2: &Section) -> bool {
+	return pair1.min <= pair2.min && pair1.max >= pair2.max;
 }
 
 // Solution
 fn solve(input: &Vec<String>) {
-	let mut counter = 0;
+	let mut contained_counter = 0;
+	let mut overlap_counter = 0;
 	for line in input {
 		let mut line = line.split(",");
-		let mut section1 = line.next().unwrap().split("-");
-		let section1 = Section{min:section1.next().unwrap().parse().unwrap(), max:section1.next().unwrap().parse().unwrap()};
-		let mut section2 = line.next().unwrap().split("-");
-		let section2 = Section{min:section2.next().unwrap().parse().unwrap(), max:section2.next().unwrap().parse().unwrap()};
-		if (section1.min <= section2.min && section1.max >= section2.max) ||
-		(section2.min <= section1.min && section2.max >= section1.max) {
-			counter += 1;
-		}
-	}
-    println!("Part 1: {}", counter);
 
-	let mut counter = 0;
-	for line in input {
-		let mut line = line.split(",");
-		let mut section1 = line.next().unwrap().split("-");
-		let section1 = Section{min:section1.next().unwrap().parse().unwrap(), max:section1.next().unwrap().parse().unwrap()};
-		let mut section2 = line.next().unwrap().split("-");
-		let section2 = Section{min:section2.next().unwrap().parse().unwrap(), max:section2.next().unwrap().parse().unwrap()};
-		if do_overlap(section1, section2) {
-			counter += 1;
+		let mut section1 = line.next()
+			.expect("Not enough sections in line.")
+			.split("-")
+			.map(|x| x.parse::<u32>()
+				.expect(&format!("Couldn't parse {} as u32.", x)));
+		let section1 = Section{
+			min:section1.next()
+				.expect("Not enough numbers in section."), 
+			max:section1.next()
+				.expect("Not enough numbers in section.")};
+
+		let mut section2 = line.next()
+			.expect("Not enough sections in line.")
+			.split("-")
+			.map(|x| x.parse::<u32>()
+				.expect(&format!("Couldn't parse {} as u32.", x)));
+		let section2 = Section{
+			min:section2.next()
+				.expect("Not enough numbers in section."), 
+			max:section2.next()
+				.expect("Not enough numbers in section.")};
+
+		if do_overlap(&section1, &section2) {
+			overlap_counter += 1;
+			if contains(&section1, &section2) || 
+				contains(&section2, &section1) {
+				contained_counter += 1;
+			}
 		}
 	}
-    println!("Part 2: {}", counter);
+    println!("Part 1: {}", contained_counter);
+    println!("Part 2: {}", overlap_counter);
 }
